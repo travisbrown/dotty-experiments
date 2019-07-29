609c3cbbb0e83cdea71003681d46d884c0c33a36
Travis Brown <travisrobertbrown@gmail.com>
2019-07-29 23:10:47 +0100

    Note for circe-literal

--- dotty-circe/literal/src/main/scala/io/circe/literal/AsJson.scala
+++ dotty-circe/literal/src/main/scala/io/circe/literal/AsJson.scala
@@ -1,11 +1,12 @@
 package io.circe.literal
 
-import io.circe.{Encoder, Json, KeyEncoder}
+import io.circe.{Encoder, Json}
 import scala.language.implicitConversions
 
 opaque type AsJson = Json
 
 object AsJson {
+  // See https://github.com/lampepfl/dotty/issues/6914
   implicit def toAsJson[A](a: A) given (A: Encoder[A]): AsJson = A(a)
 
   given AsJsonOps {
