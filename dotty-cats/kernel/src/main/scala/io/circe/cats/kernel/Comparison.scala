c75d311ea62fee7f236918eefa61cc4a8fd542f4
Travis Brown <travisrobertbrown@gmail.com>
2019-07-30 18:01:56 +0100

    Revert "Use enum for Comparison"
    This reverts commit 67e9742f2b809af7de76958e830e1cbde333053e.

--- dotty-cats/kernel/src/main/scala/io/circe/cats/kernel/Comparison.scala
+++ dotty-cats/kernel/src/main/scala/io/circe/cats/kernel/Comparison.scala
@@ -1,13 +1,13 @@
 package io.circe.cats.kernel
 
 /** ADT encoding the possible results of a comparison */
-enum Comparison(val toInt: Int, val toDouble: Double) {
-  case GreaterThan extends Comparison(1, 1.0)
-  case EqualTo extends Comparison(0, 0.0)
-  case LessThan extends Comparison(-1, -1.0)
-}
+sealed abstract class Comparison(val toInt: Int, val toDouble: Double) extends Product with Serializable
 
 object Comparison {
+  case object GreaterThan extends Comparison(1, 1.0)
+  case object EqualTo extends Comparison(0, 0.0)
+  case object LessThan extends Comparison(-1, -1.0)
+
   // Used for fromDouble
   private val SomeGt = Some(Comparison.GreaterThan)
   private val SomeEq = Some(Comparison.EqualTo)
@@ -24,8 +24,5 @@ object Comparison {
     else if (double == 0.0) SomeEq
     else SomeLt
 
-  // Eq.fromUniversalEquals crashes with `java.lang.VerifyError: Bad type on operand stack`.
-  given as Eq[Comparison] {
-    def eqv(x: Comparison, y: Comparison): Boolean = x == y
-  }
+  given as Eq[Comparison] = Eq.fromUniversalEquals
 }
